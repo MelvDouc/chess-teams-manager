6b914debb62683711283346a9fd063bb2ffdd718
MelvDouc <melv.douc@gmail.com>
2023-04-23 01:13:35 +0200

    mysql types

--- server/types.ts
+++ server/types.ts
@@ -4,8 +4,9 @@ export type Nullable<T> = {
   [K in keyof T]: T[K] | null;
 };
 
-export namespace DbEntities {
+export namespace MySqlEntities {
   export interface Player {
+    /** @primaryKey */
     ffe_id: string;
     fide_id: number | null;
     email: string;
@@ -16,18 +17,64 @@ export namespace DbEntities {
   }
 
   export interface Team {
+    /** @primaryKey */
     id: number;
     name: string;
-    captain: Player;
+    captain_ffe_id: Player["ffe_id"];
   }
 
+  export type TeamWithCaptain = {
+    team_id: Team["id"];
+    team_name: Team["name"];
+  } & {
+      [K in keyof Player as `captain_${K}`]: Player[K]
+    };
+
   export interface Club {
+    /** @primaryKey */
     id: number;
     name: string;
     address: string;
     phone: string | null;
     email: string | null;
   }
+
+  export interface Match {
+    /** @primaryKey */
+    id: number;
+    season: number;
+    round: number;
+    team_id: Team["id"];
+    opponent_id: Club["id"];
+    home_club_id: Club["id"];
+    white_on_odds: 0 | 1;
+    date: string;
+  }
+
+  export interface LineUp {
+    board: number;
+    player_ffe_id: Player["ffe_id"];
+    match_id: Match["id"];
+    player_rating: Player["rating"];
+  }
+
+  export interface User {
+    /** @primaryKey */
+    email: string;
+    role: "ADMIN" | "CAPTAIN" | "USER";
+    password: string;
+    password_reset_id: string | null;
+  }
+}
+
+export namespace DbEntities {
+  export type Player = MySqlEntities.Player;
+
+  export type Team = Omit<MySqlEntities.Team, "captain_ffe_id"> & {
+    captain: Player;
+  };
+
+  export type Club = MySqlEntities.Club;
 
   export interface Match {
     id: number;
@@ -46,12 +93,7 @@ export namespace DbEntities {
     player: Player | null;
   }[];
 
-  export interface User {
-    email: string;
-    role: "ADMIN" | "CAPTAIN" | "USER";
-    password: string;
-    password_reset_id: string | null;
-  }
+  export type User = MySqlEntities.User;
 }
 
 export type BoardColor = "B" | "N";
