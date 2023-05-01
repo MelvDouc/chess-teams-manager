8423faf6982eebecb06e8448aeb5ef6af2a8d739
MelvDouc <melv.douc@gmail.com>
2023-05-01 11:55:57 +0200

    Fix regex

--- server/src/services/templates.service.ts
+++ server/src/services/templates.service.ts
@@ -1,8 +1,16 @@
 import { join } from "node:path";
 import { readFile } from "node:fs/promises";
 
+const interpolationRegex = RegExp("{{\\s*(\\S+)\\s*}}", "g");
+const fileContentCache = new Map<string, string>();
+
 export async function compileTemplate(templateName: string, ctx: Record<string, any>) {
-  const fileContents = await readFile(join(process.cwd(), "server", "templates", `${templateName}.html`), "utf-8");
+  const fileContents = fileContentCache.get(templateName)
+    ?? await readFile(join(process.cwd(), "server", "templates", `${templateName}.html`), "utf-8");
+
+  if (!fileContentCache.has(templateName))
+    fileContentCache.set(templateName, fileContents);
+
   const html = addContext(fileContents, ctx);
 
   return {
@@ -12,7 +20,7 @@ export async function compileTemplate(templateName: string, ctx: Record<string, 
 }
 
 function addContext(fileContents: string, ctx: Record<string, any>): string {
-  return fileContents.replace(/\{\{\s*([^\s]+)\s*\}\}/g, (_, key) => ctx[key] ?? "");
+  return fileContents.replace(interpolationRegex, (_, key) => ctx[key] ?? "");
 }
 
 function stripTags(html: string): string {
