9c8ae000777eef3a62a0553542de21f34a0cc976
MelvDouc <melv.douc@gmail.com>
2023-05-04 08:06:11 +0200

    Fix bootstrap imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import Header from "@src/components/Header/Header.jsx";
 import Modal from "@src/components/Modal/Modal.jsx";
 import router from "@src/router.jsx";
 import auth from "@src/utils/auth.js";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 
 export default function App() {
   return {
