bbfef6d7f254bd2d491d7d8be78ddbaee1a4ce13
MelvDouc <melv.douc@gmail.com>
2023-05-07 10:35:48 +0200

    Use env vars

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -3,7 +3,9 @@
 // import("reactfree-jsx");
 
 interface ImportMetaEnv {
+  readonly VITE_LOGO_URL: string;
   readonly VITE_SERVER_URL: string;
+  readonly VITE_SITE_TITLE: string;
 }
 
 interface ImportMeta {
